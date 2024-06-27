c4caceb06bc8ac5dc32a98087e55d1ed965f1441
abraham-shimelis <156053086+abraham-shimelis@users.noreply.github.com>
2024-06-27 23:17:36 +0300

    build predict endpoint using flask

---
+++ fraud_detection_api/serve_model.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('../')
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+from preprocess_input import preprocess_input
+
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load("../models/rfc_model.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the data from the request
+    data = request.get_json(force=True)
+    
+    # Preprocess the input
+    preprocessed_data = preprocess_input(data)
+    
+    # Make a prediction
+    prediction = model.predict(preprocessed_data)
+    
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
